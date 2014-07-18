5fb103d183e5a6ab2a9bc0d7edf1c8ae524feea9
Jared Forsyth <jared@khanacademy.org>
2014-07-17 21:06:51 -0700

    refactor

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 
-var Bakhan = require('./lib')
+var bakhan = require('./lib')
   , node = document.getElementById('main-canvas')
 
-new Bakhan(node).run();
+new bakhan.Demo(node, {width: 900, height: 500}).run();
 
