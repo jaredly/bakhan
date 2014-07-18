8cb37294530c3faec5ead9cfbf5ee448f29e11f3
Ben Kraft <benkraft@khanacademy.org>
2014-07-17 22:28:08 -0700

    adds initial orbit demo

--- run.js
+++ run.js
@@ -2,5 +2,9 @@
 var bakhan = require('./lib')
   , node = document.getElementById('main-canvas')
 
-window.BKA = new bakhan.Demo(node, {width: 900, height: 500});
+if (window.location.search.indexOf('orbit') > -1) {
+    window.BKA = new bakhan.Orbit(node, {width: 1400, height: 700});
+} else {
+    window.BKA = new bakhan.Demo(node, {width: 900, height: 500});
+}
 window.BKA.run();
