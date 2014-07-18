150199112bf72877e9bbf7cd74155cbfb631e9a6
Miles Steele <miles@khanacademy.org>
2014-07-17 21:52:25 -0700

    add a global, woohooo

--- run.js
+++ run.js
@@ -2,5 +2,5 @@
 var bakhan = require('./lib')
   , node = document.getElementById('main-canvas')
 
-new bakhan.Demo(node, {width: 900, height: 500}).run();
-
+window.BKA = new bakhan.Demo(node, {width: 900, height: 500});
+window.BKA.run();
