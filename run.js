4d0c68d1fc43a0485c97a9cb89f910b69288f4d8
Miles Steele <miles@khanacademy.org>
2014-07-18 16:40:36 -0700

    shrink arena

--- run.js
+++ run.js
@@ -2,5 +2,5 @@
 var bakhan = require('./lib')
   , node = document.getElementById('main-canvas')
 
-window.BKA = new bakhan[window.location.search.slice(1) || 'Demo'](node, {width: 1400, height: 700});
+window.BKA = new bakhan[window.location.search.slice(1) || 'Demo'](node, {width: 900, height: 700});
 window.BKA.run();
