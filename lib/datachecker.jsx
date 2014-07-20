a963d8d824811e72ab06ba4a55f64d09b6d85c44
Ben Kraft <benkraft@khanacademy.org>
2014-07-19 21:52:42 -0700

    adds generalized data checker

---
+++ lib/datachecker.jsx
@@ -0,0 +1,97 @@
+/** @jsx React.DOM */
+
+var DataChecker = React.createClass({
+    propTypes: {
+        logBook: React.PropTypes.object.isRequired,
+        initialText: React.PropTypes.string.isRequired,
+        initialHypothesis: React.PropTypes.string.isRequired,
+        possibleHypotheses: React.PropTypes.objectOf(React.PropTypes.shape({
+            name: React.PropTypes.string.isRequired,
+            buttonText: React.PropTypes.string.isRequired, // the text on the button to change your hypothesis
+            text: React.PropTypes.string.isRequired, // "Your hypothesis was <text>."
+        })).isRequired,
+        result: React.PropTypes.func.isRequired, // takes in a logbook, and returns an error string for francis to say, or null if there are no problems with the experiment.
+    },
+
+    getInitialState: function () {
+        return {
+            thisResult: this.props.initialText,
+            prevResult: '',
+            hypothesis: this.props.initialHypothesis, // a hypothesis.name
+            disproven: false,
+        };
+    },
+
+    renderHypothesis: function () {
+        return <p className="checker_your-hypo">
+            <em>Your hypothesis was {this.props.possibleHypotheses[this.state.hypothesis].text}.</em>
+        </p>
+    }
+
+    render: function () {
+        if (this.state.disproven) {
+            var buttons = _.map(
+                _.filter(
+                    this.props.possibleHypotheses,
+                    function (hyp) {
+                        return (this.state.hypothesis !== hyp.name);
+                    }),
+                function (hyp) {
+                    return <button key={hyp.name} className="btn btn-default" onClick={function () {this.changeHypothesis(hyp.name)}}>{hyp.buttonText}</button>;
+                });
+
+            return <div className="checker">
+                {this.renderHypothesis()}
+                <img src="images/sir-francis.jpeg" className="checker_francis"/>
+                <div className="checker_main">
+                    <p>Okay, which result do they support?</p>
+                    {buttons}
+                </div>
+            </div>;
+        } else if (this.state.thisResult) {
+            return <div className="checker">
+                {this.renderHypothesis()}
+                <img src="images/sir-francis.jpeg" className="checker_francis"/>
+                <div className="checker_main">
+                    <p>{this.state.thisResult}</p>
+                    <button className="btn btn-default" onClick={this.support}>The data support my hypothesis.</button>
+                    <button className="btn btn-default" onClick={this.disprove}>The data disprove my hypothesis.</button>
+                </div>
+            </div>;
+        } else {
+            return <div className="checker">
+                {this.renderHypothesis()}
+                <img src="images/sir-francis.jpeg" className="checker_francis"/>
+                <div className="checker_main">
+                    <p>Your experiment looks great, and I'm convinced.  Here, have some bacon.</p>
+                </div>
+            </div>;
+        }
+    },
+
+    support: function () {
+        this.askFrancis();
+    },
+
+    disprove: function () {
+        this.setState({
+            disproven: true,
+        });
+    },
+
+    changeHypothesis: function (hyp) {
+        this.setState({
+            disproven: false,
+            hypothesis: hyp.name,
+        }, this.askFrancis);
+    },
+
+    askFrancis: function () {
+        this.setState({
+            thisResult: this.result(),
+            prevResult: this.state.thisResult
+        });
+    }
+})
+
+module.exports = DataChecker;
